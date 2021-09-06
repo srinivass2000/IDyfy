d66ba230530ffb1add49ce7cc9a640c8ab0f043e
udandeprempvs <premudande768@gmail.com>
2021-09-07 00:34:36 +0530

    assets folder added and tailwind cdn added

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Idyfy_logo from '../../assets/svg/Idyfy_logo.svg';
+import Idyfy_name from '../../assets/svg/Idyfy_name.svg';
+
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="flex flex-row">
+        <div><img src={Idyfy_logo} alt="IDYFY LOGO"/></div>
+        <div><img src={Idyfy_name} alt="IDYFY NAME"/></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
