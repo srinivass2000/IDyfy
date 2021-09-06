e3dcdf952ebd958fb37f8fb2e4e887963888c6a3
lex03 <lexmascarenhas3@gmail.com>
2021-09-07 00:35:52 +0530

    starred Ideas

--- client/src/components/Feed_Tile/feed.js
+++ client/src/components/Feed_Tile/feed.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const FeedTile = () => {
-  return <div>Feed</div>;
+  return (
+    <div className="border">
+      <div className="row">
+        <div className="offset-1 mt-5 col-8">
+          <div className="row">Title</div>
+          <div className="row">Field</div>
+        </div>
+
+        <div className="col-3 mt-5">
+          <div className="rounded-circle">
+            <h1>5</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedTile;
