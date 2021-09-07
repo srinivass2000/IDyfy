43683c13f53fb8b209224576c2f15b0ed2f31704
lex03 <lexmascarenhas3@gmail.com>
2021-09-07 09:56:13 +0530

    Final Feed Tile

--- client/src/components/Feed_Tile/feed.js
+++ client/src/components/Feed_Tile/feed.js
@@ -1,13 +1,18 @@
 import React from "react";
 import FeedIcon from "../../assets/svg/feedicon.svg";
+import Star from "../../assets/icon/star.svg";
+import Chat from "../../assets/icon/chat.svg";
+import Share from "../../assets/icon/share.svg";
+import Like from "../../assets/icon/heart.svg";
+
 import "../Feed_Tile/feed.css";
 const FeedTile = () => {
   return (
     <div className="text-white p-4 tile">
       <div className="row">
         <div className="offset-1 mt-5 col-8">
-          <div className="row">Title</div>
-          <div className="row">Field</div>
+          <div className="row">Title : Ideas of Ideas</div>
+          <div className="row">Field : ML and Web</div>
         </div>
 
         <div className="col-3 mt-3 relative">
@@ -27,17 +32,38 @@ const FeedTile = () => {
       <div className="row mt-3">
         <h2>Description</h2>
         <p className="mt-1">
-          lorem ipsum desve ge gergerk eirj ijf jrjfi w ff if f wjfwf wi we
-          wfwjfwnfnwf jowjf wf{" "}
+          Our platform will bring like minded people to work for a common goal
+          without much friction and make the whole brainstorming part of the
+          ideation phase effortless.
         </p>
       </div>
       <button className="my-3 readmore rounded-pill px-2">Read more</button>
       <hr className="hr mt-1" />
       <div className="row mt-3">
-        <div className="offset-1 col-1">Like</div>
-        <div className="col-1">Chat</div>
-        <div className="col-1">Star</div>
-        <div className="offset-6 col-1">Share</div>
+        <div className="offset-1 col-2 ">
+          <img src={Like} alt="feed icon" className="absolute mt-1" /> 3
+        </div>
+        <div className="col-2">
+          <img
+            src={Chat}
+            alt="feed icon"
+            className="absolute mr-4 mt-1"
+            width="22px"
+          />{" "}
+          20
+        </div>
+        <div className="col-2">
+          <img
+            src={Star}
+            alt="feed icon"
+            className="absolute mt-1 "
+            width="32px"
+          />{" "}
+          5
+        </div>
+        <div className="offset-4 col-1">
+          <img src={Share} alt="feed icon" className="mt-1" />
+        </div>
       </div>
     </div>
   );
