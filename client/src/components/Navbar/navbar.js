d328c972ccb69c288726b7c7145e2662c832dae0
udandeprempvs <premudande768@gmail.com>
2021-09-07 10:18:48 +0530

    navigation bar conflict fixed

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -16,37 +16,37 @@ const Navbar = () => {
       <div className="flex content-center" style=
       {{ backgroundColor : "#B287FF"}}>
         <div className="ml-5"><img src={Idyfy_logo} alt="IDYFY LOGO"/></div>
-        <div className="flex-auto pt-5 ml-3"><img src={Idyfy_name} alt="IDYFY NAME"/></div>
-        <div className="relative pt-5"> 
+        <div className="flex-auto pt-4 ml-3"><img src={Idyfy_name} alt="IDYFY NAME"/></div>
+        <div className="relative pt-4"> 
           <input type="text" className="h-14 w-96 pr-8 pl-5 rounded-xl z-0 focus:shadow focus:outline-none border" placeholder="Search for Ideas..."/>
           <div className="absolute top-4 right-3 pt-6"> <img src={search} alt="Search"/> </div>
         </div>
         <div className="flex-auto flex flex-col">
           <Link to="/">
-          <div className="flex justify-center pt-5"><img src={home} alt="home"/></div>
-          <div className="pt-3"><h6>Home</h6></div>
+          <div className="flex justify-center pt-4"><img src={home} alt="home"/></div>
+          <div className="pt-2"><h6>Home</h6></div>
           </Link>
         </div>
         <div className="flex-auto flex flex-col">
-          <div className="flex justify-center pt-5"><img src={new_idea} alt="New Idea"/></div>
+          <div className="flex justify-center pt-4"><img src={new_idea} alt="New Idea"/></div>
           <div><h6>New Idea</h6></div>
         </div>
         <div className="flex-auto flex flex-col">
         <Link to="/myideas">
-          <div className="flex justify-center pt-3"><img src={my_idea} alt="My Ideas"/></div>
+          <div className="flex justify-center pt-4"><img src={my_idea} alt="My Ideas"/></div>
           <div><h6>My Ideas</h6></div>
           </Link>
         </div>
         
         <div className="flex-auto flex flex-col">
         <Link to="/starred">
-          <div className="flex justify-center pt-5"><img src={starred} alt="Starred Ideas"/></div>
+          <div className="flex justify-center pt-4"><img src={starred} alt="Starred Ideas"/></div>
           <div><h6>Starred Ideas</h6></div>
           </Link>
         </div>
         <div className="flex-auto flex flex-col">
         <Link to="/notifiction">
-          <div className="flex justify-center pt-5"><img src={notifications} alt="Notifications"/></div>
+          <div className="flex justify-center pt-4"><img src={notifications} alt="Notifications"/></div>
           <div><h6>Notifications</h6></div>
           </Link>
         </div>
