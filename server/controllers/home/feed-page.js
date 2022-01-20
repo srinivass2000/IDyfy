5245bde3507b49b02dd0d3203c8fa06d0d390514
Srinivass Talaulikar <talaulikarsrinivass@outlook.com>
2022-01-20 23:25:24 +0530

    ASync await Skipping && Profile Api Updated for other's Perspective :)

--- server/controllers/home/feed-page.js
+++ server/controllers/home/feed-page.js
@@ -2,24 +2,25 @@ const Idea = require("../../models/Idea");
 const ErrorResponse = require("../../utils/errorResponse");
 const Comment = require("../../models/Comment");
 
-const get_comments = async (idea) => {
-  // var comment_count;
-  // result.forEach(async (idea) => {
-  var idea_id = idea._id;
-  idea_id = idea_id.toString();
-  console.log(idea_id);
-  const comment_count = await Comment.find({
-    idea_id: idea_id,
-    feature_id: null,
-  }).count();
-
-  console.log(comment_count);
-  // Object.assign(comments, { [idea_id]: comment_count });
-  // comments.idea_id = comment_count;
-  // console.log(comments);
-  return comment_count;
-  // });
-};
+async function get_comments(result) {
+  var comments_final = [];
+  result.map(async (idea, i) => {
+    // console.log(idea);
+    var idea_id = idea._id;
+    idea_id = idea_id.toString();
+    // console.log(idea_id);
+    var comment_count = await Comment.find({
+      idea_id: idea_id,
+      feature_id: null,
+    }).count();
+    // console.log(idea);
+    // console.log(comment_count);
+    comments_final.push(comment_count);
+    console.log(comments_final);
+  });
+  console.log(comments_final);
+  return comments_final;
+}
 
 exports.get_ideas = async (req, res, next) => {
   try {
@@ -34,36 +35,20 @@ exports.get_ideas = async (req, res, next) => {
       .skip(skip)
       .limit(PAGE_SIZE);
 
-    // console.log(result);
-    // const comments = get_comments(result);
-    comments_final = {};
-    global.me = {};
-    console.log(result);
-    const comments = result.forEach(async (idea) => {
-      // var comment_count = get_comments();
-      var idea_id = idea._id;
-      idea_id = idea_id.toString();
-      console.log(idea_id);
-      const comment_count = await Comment.find({
-        idea_id: idea_id,
-        feature_id: null,
-      }).count();
-      // console.log(idea);
-      // console.log(comment_count);
-      Object.assign(comments_final, { [idea_id]: comment_count });
-      // console.log(comments_final);
-      // return comments_final;
-      // global.me = comments;
-    });
+    await execute(result);
+    async function execute(result) {
+      try {
+        const comments = await get_comments(result);
 
-    console.log(comments);
-    console.log(comments_final);
-    console.log(me);
-    res.status(200).json({
-      success: true,
-      ideas: result,
-      comments: comments_final,
-    });
+        res.status(200).json({
+          success: true,
+          ideas: result,
+          comments: comments,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
   } catch (err) {
     console.log(err);
     return next(new ErrorResponse(err.message, 500));
--- server/controllers/home/get-profile.js
+++ server/controllers/home/get-profile.js
@@ -1,18 +1,27 @@
 const Idea = require("../../models/Idea");
+const User = require("../../models/User");
 const ErrorResponse = require("../../utils/errorResponse");
-const { protect } = require("../../middleware/auth");
+// const { protect } = require("../../middleware/auth");
 
 exports.get_profile = async (req, res, next) => {
   try {
-    console.log(req.user._id);
-    const user = req.user;
-    var id = req.user._id;
-    id = id.toString();
+    user_id = req.query.id;
+    var user, id;
+    if (user_id) {
+      user = await User.findById(user_id);
+      id = user_id;
+    } else {
+      console.log(req.user._id);
+      user = req.user;
+      id = req.user._id;
+      id = id.toString();
+    }
+    //user specific using query
     const result = await Idea.find({
       contributors: {
         $in: [id],
       },
-    });
+    }).sort({ createdAt: -1 });
     res.status(200).json({
       success: true,
       user,
