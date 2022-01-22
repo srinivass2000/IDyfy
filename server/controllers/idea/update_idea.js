36c0ce2a9fae921667fa1267a6f102666dd654a0
Srinivass Talaulikar <talaulikarsrinivass@outlook.com>
2022-01-22 23:26:23 +0530

    Links in edit idea updated

--- server/controllers/idea/update_idea.js
+++ server/controllers/idea/update_idea.js
@@ -4,13 +4,9 @@ const ErrorResponse = require("../../utils/errorResponse");
 
 exports.update_idea = async (req, res, next) => {
   try {
-    const { title, description, tags, link_figma, link_github, idea_id } =
-      req.body;
+    const { title, description, tags, links, idea_id } = req.body;
     var idea = await Idea.findById(idea_id);
     var user_id = req.user._id.toString();
-    var links = {};
-    links["figma"] = link_figma;
-    links["github"] = link_github;
     if (idea.contributors.includes(user_id)) {
       idea = await Idea.findByIdAndUpdate(idea_id, {
         title,
