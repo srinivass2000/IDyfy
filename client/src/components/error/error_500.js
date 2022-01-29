3f73c0de77da6730c4ef5dbcd45092c373c80701
Srinivass Talaulikar <talaulikarsrinivass@outlook.com>
2022-01-29 09:28:20 +0530

    Abuse detection model added (not final.....changes needed)

---
+++ client/src/components/error/error_500.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import error from "../../assets/svg/404.svg";
+import "../Feed_Tile/feed.css";
+import { Link } from "react-router-dom";
+import Footer from "../Footer/footer";
+import "./error_500.css";
+const Error_500 = () => {
+  return (
+    <>
+      <Footer />
+    </>
+  );
+};
+export default Error_500;
--- server/routes/fake_routes.js
+++ server/routes/fake_routes.js
@@ -7,9 +7,13 @@ const { fake_features } = require("../utils/fake data/faker-features");
 
 const { testing } = require("../utils/testing");
 
+// const { testing_model } = require("../utils/model_testing");
+
 router.route("/create-fake-ideas").post(fake_idea);
 router.route("/create-fake-features").post(fake_features);
 
 router.route("/testing").post(testing);
+
+// router.route("/testing-model").post(testing_model);
 
 module.exports = router;
