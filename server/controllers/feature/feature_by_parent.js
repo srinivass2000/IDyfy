573c08455bf8f86f66ec1b0b4fd2797b1270797f
Srinivass Talaulikar <talaulikarsrinivass@outlook.com>
2022-03-23 23:32:22 +0530

    feature by parent done

--- server/controllers/feature/feature_by_parent.js
+++ server/controllers/feature/feature_by_parent.js
@@ -11,22 +11,43 @@ exports.fetch_features_by_parent = async (req, res, next) => {
       parent_id = idea_id;
     }
 
-    console.log(idea_id);
-
     var Feature = mongoose.model(`features_${idea_id}`, FeatureSchema);
 
-    console.log(Feature);
+    var idea = await Idea.findById(idea_id, { title: true });
+
+    obj2 = { show: true };
+
+    idea = { ...idea._doc, ...obj2 };
+
+    console.log(idea);
 
     var results = await Feature.find(
       {
         parent_id,
       },
-      { content: 0, content_hash: 0 }
+      { title: 1, parent_id: 1, version_start: 1, version_end: 1, available: 1 }
     );
 
+    if (results == []) {
+      return next(
+        new ErrorResponse("This Parent does'nt have any children", 204)
+      );
+    }
+    var array = [];
+
+    obj3 = { show: false };
+
+    results.forEach(function (feature) {
+      result = { ...feature._doc, ...obj3 };
+      array.push(result);
+    });
+
+    array.unshift(idea);
+
+    console.log("here");
     res.status(200).json({
       success: true,
-      features: results,
+      features: array,
     });
   } catch (err) {
     console.log(err);
