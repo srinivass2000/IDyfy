02f3860602701f7d1763a64fa8e5a084df05fef6
Srinivass Talaulikar <talaulikarsrinivass@outlook.com>
2022-03-25 12:24:37 +0530

    bug fixes in by parent

--- server/controllers/feature/feature_by_parent.js
+++ server/controllers/feature/feature_by_parent.js
@@ -11,6 +11,21 @@ exports.fetch_features_by_parent = async (req, res, next) => {
       parent_id = idea_id;
     }
 
+    if (parent_id == idea_id) {
+      if (idea_id != null) {
+        var idea = await Idea.findById(idea_id, { title: true });
+
+        obj2 = { show: false };
+
+        idea = { ...idea._doc, ...obj2 };
+
+        return res.status(200).json({
+          success: true,
+          features: idea,
+        });
+      }
+    }
+
     //if idea_id isequal to parent_id
 
     var Feature = mongoose.model(`features_${idea_id}`, FeatureSchema);
@@ -29,8 +44,9 @@ exports.fetch_features_by_parent = async (req, res, next) => {
       }
     );
 
+    console.log("initial" + results);
     if (results.length == 0) {
-      return res.status(200).json({
+      return res.status(204).json({
         success: false,
         note: "This Parent does'nt have any children",
       });
@@ -40,24 +56,38 @@ exports.fetch_features_by_parent = async (req, res, next) => {
     var array = [];
 
     obj3 = { show: false };
+    obj4 = { show: "nothing" };
 
-    results.forEach(function (feature) {
-      result = { ...feature._doc, ...obj3 };
+    results.forEach(async function (feature) {
+      // console.log(feature);
+      // var test = await Feature.find(
+      //   {
+      //     parent_id: feature._id.toString(),
+      //   },
+      //   {
+      //     _id: 1,
+      //   }
+      // );
+      // if (test.length == 0) {
+      result = { ...feature._doc, ...obj4 };
+      // } else {
+      //   result = { ...feature._doc, ...obj3 };
+      // }
       array.push(result);
     });
 
-    if (parent_id == idea_id) {
-      if (idea_id != null) {
-        var idea = await Idea.findById(idea_id, { title: true });
+    // if (parent_id == idea_id) {
+    //   if (idea_id != null) {
+    //     var idea = await Idea.findById(idea_id, { title: true });
 
-        obj2 = { show: true };
+    //     obj2 = { show: true };
 
-        idea = { ...idea._doc, ...obj2 };
+    //     idea = { ...idea._doc, ...obj2 };
 
-        console.log(idea);
-        array.unshift(idea);
-      }
-    }
+    //     console.log(idea);
+    //     array.unshift(idea);
+    //   }
+    // }
 
     console.log("here");
     res.status(200).json({
