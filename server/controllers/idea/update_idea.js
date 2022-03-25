f970d4459329ddca32cc7984d815da61b8f7296d
Srinivass Talaulikar <talaulikarsrinivass@outlook.com>
2022-03-25 22:04:36 +0530

    Multiple File uploads done

--- server/controllers/idea/update_idea.js
+++ server/controllers/idea/update_idea.js
@@ -10,58 +10,65 @@ exports.update_idea = async (req, res, next) => {
     var idea = await Idea.findById(idea_id);
     var user_id = req.user._id.toString();
     if (idea.contributors.includes(user_id)) {
-      if (req.file) {
-        console.log(req.file.filename);
-        var name = req.file.filename;
-        name = name.replace(/\s+/g, "");
-        console.log(name);
-        const split = name.split(".");
-        const extension = split[1].toLowerCase();
-        console.log(extension);
-        if (
-          extension !== "jpg" &&
-          extension !== "jpeg" &&
-          extension !== "png" &&
-          extension !== "pdf" &&
-          extension !== "doc" &&
-          extension !== "docx"
-        ) {
-          res.status(405).json({
-            success: false,
-            error: `${extension} is not a valid extension`,
-          });
-        }
+      console.log(req.files);
+      var files = req.files;
 
-        if (req.file) {
-          idea = await Idea.findById(idea_id);
-          console.log(idea.documents);
-          var document = {};
-          // const imgUrl = `https://idyfy.tech/file/${req.file.filename}`;
-          document[`${req.file.originalname}`] = req.file.filename;
-          idea.documents.push(document);
-          idea = await Idea.findByIdAndUpdate(idea_id, {
-            title,
-            description,
-            tags,
-            links,
-            documents: idea.documents,
-          });
-        } else {
-          idea = await Idea.findByIdAndUpdate(idea_id, {
-            title,
-            description,
-            tags,
-            links,
-          });
-        }
-
-        idea = await Idea.findById(idea_id);
-
-        res.status(200).json({
-          success: true,
-          idea,
+      idea = await Idea.findById(idea_id);
+      // console.log(idea.documents);
+      if (idea.documents.length > 0) {
+        var document = idea.documents[0];
+      } else {
+        var document = {};
+      }
+      if (req.files) {
+        // const imgUrl = `https://idyfy.tech/file/${req.file.filename}`;
+        files.filter(function (file) {
+          console.log(file.filename);
+          var name = file.filename;
+          name = name.replace(/\s+/g, "");
+          console.log(name);
+          const split = name.split(".");
+          const extension = split[1].toLowerCase();
+          console.log(extension);
+          if (
+            extension !== "jpg" &&
+            extension !== "jpeg" &&
+            extension !== "png" &&
+            extension !== "pdf" &&
+            extension !== "doc" &&
+            extension !== "docx"
+          ) {
+            res.status(405).json({
+              success: false,
+              error: `${extension} is not a valid extension`,
+            });
+          }
+          document[`${file.originalname}`] = file.filename;
+          // idea.documents[0].push(document);
+          console.log("inserted");
+        });
+        idea = await Idea.findByIdAndUpdate(idea_id, {
+          title,
+          description,
+          tags,
+          links,
+          documents: document,
+        });
+      } else {
+        idea = await Idea.findByIdAndUpdate(idea_id, {
+          title,
+          description,
+          tags,
+          links,
         });
       }
+
+      idea = await Idea.findById(idea_id);
+
+      res.status(200).json({
+        success: true,
+        idea,
+      });
     } else {
       return next(
         new ErrorResponse("You are not a contributor to this Idea", 403)
--- server/routes/idea_routes.js
+++ server/routes/idea_routes.js
@@ -24,7 +24,7 @@ const {
 
 router.route("/create-idea").post(protect, create_idea);
 router.route("/get-idea").get(protect, get_idea);
-router.route("/update-idea").put(protect, upload.single("file"), update_idea);
+router.route("/update-idea").put(protect, upload.array("file", 5), update_idea);
 router.route("/get-ideas/:skip").get(protect, get_ideas);
 router.route("/get-contributed-ideas").get(protect, get_contributed_ideas);
 router.route("/star-idea").get(protect, star_idea);
