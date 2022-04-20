3ec51cb4f4c1d2ccd6bfb2fb19c58b3afa32b613
Srinivass Talaulikar <talaulikarsrinivass@outlook.com>
2022-04-21 00:11:13 +0530

    Stable review commit

--- server/controllers/graph/feature_by_parent.js
+++ server/controllers/graph/feature_by_parent.js
@@ -122,7 +122,7 @@ exports.fetch_features_by_parent = async (req, res, next) => {
         return res.status(200).json({
           success: true,
           features: [idea],
-          whose_id: user._id.toString(),
+          whose_id: user.toString(),
         });
       }
     }
